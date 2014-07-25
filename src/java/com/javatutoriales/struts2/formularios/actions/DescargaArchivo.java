70c52fe24bde82d37c1ffff6a70b48620b2d9966
InmaGs <inmac.garciasanchez@gmail.com>
2014-07-25 12:01:01 +0200

    tercera parte descarga de archvos

--- src/java/com/javatutoriales/struts2/formularios/actions/DescargaArchivo.java
+++ src/java/com/javatutoriales/struts2/formularios/actions/DescargaArchivo.java
@@ -11,6 +11,7 @@ package com.javatutoriales.struts2.formularios.actions;
 
 import java.io.FileInputStream; 
 import java.io.InputStream; 
+import java.io.File;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.convention.annotation.*;
 import org.apache.struts2.ServletActionContext; 
@@ -25,9 +26,15 @@ import org.apache.struts2.ServletActionContext;
 // sea descargado añadimos el contentDisposition incluyendo el nombre que tomará el archivo
 // en el momento de la descarga. Si no se especifica, el archivo se visualizará
 // en el navegador.
+// Agregamos también el parámetro que nos idicará el tamaño del archivo
 @Action(value="descargaArchivo", results={@Result(type="stream", params=
         {"contentType","application/pdf",
-         "contentDisposition","attachment;filename=\"tutorial.pdf\""})}) 
+         "contentDisposition","attachment;filename=\"tutorial.pdf\"",
+         "contentLenght","${bytesArchivo}"})})
+//Aquí se ha puesto un nombre estático para la descarga del documento. Se puede poner 
+//dinámicamente a través de filename=\"${nombreArchivo}"\, siendo nombreArchivo el 
+//nombre del archivo que se obtendrá a través de su getter correspondiente, igual que
+//ocurre con bytesArchivo.
 public class DescargaArchivo extends ActionSupport {
     
     private InputStream inputStream; 
@@ -35,12 +42,27 @@ public class DescargaArchivo extends ActionSupport {
     public String execute() throws Exception{
 
         String ruta=ServletActionContext.getServletContext().getRealPath("/Ingenieria de software.pdf");
+        
+        // Declaramos un archivo de tipo File que obtiene la ruta del archivo y el FileInputStream lo que lee
+        // es un objeto que representa al archivo
+        File archivo=new File(ruta);
+        //Obtenemos la longitud del archivo:
+        bytesArchivo= archivo.length();
+        
         inputStream=new FileInputStream(ruta);
+                
         return SUCCESS;
     }
 
     public InputStream getInputStream(){
         return inputStream;
+    }
+    
+    //Declaramos la variable y el getter que va a obtener el tamaño del archivo
+    private long bytesArchivo;
+    
+    public long getBytesArchivo(){
+        return bytesArchivo;
     }
     
 }
